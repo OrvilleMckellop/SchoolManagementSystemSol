728c68920e97294da018fa29dcd0a7a69fd52468
Danielmichaelpalmer15@gmail.com <Danielmichaelpalmer15@gmail.com>
2022-08-15 20:33:48 -0700

    changes

--- SchoolManagementSystem/Library.aspx.cs
+++ SchoolManagementSystem/Library.aspx.cs
@@ -12,18 +12,47 @@ namespace SchoolManagementSystem
 {
     public partial class Library : System.Web.UI.Page
     {
-        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBschoolmanagementsystemConnectionString"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (con.State == ConnectionState.Open);
+            {
+                con.Close();
+            }
             con.Open();
+            disp_data();
         }
 
-       
-
-        protected void Button1_Click(object sender, EventArgs e)
+        protected void Button2_Click(object sender, EventArgs e)
         {
-            SqlCommand cmd = new SqlCommand("View into libr values('"+TextBox1+"')")
-       
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into Library Values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text+ "','" + TextBox4.Text + "','" + TextBox5.Text + "','" + TextBox6.Text + "')";
+            cmd.ExecuteNonQuery();
+
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+
+            disp_data();
+
+        }
+
+        public void disp_data()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select * from Library";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSourceID = String.Empty;
+            GridView1.DataBind();
+           
 
         }
     }
